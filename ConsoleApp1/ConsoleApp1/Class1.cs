fb97227d8a9e15b28d1344f44d82ce6ecbd3258e
Laurynas Danius <laurynas.danius1@gmail.com>
2022-02-28 08:26:33 +0200

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Uzd1
+{
+    internal class Pagrindinis
+    {
+        static void Main(string[] args)
+        {   //Laiko skaiciavimui
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Matematika matematika = new Matematika(Math.Pow(10, 15), 1, 2);
+            matematika.Skaiciavimas();
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            Console.WriteLine("Praejes laikas (Valandos:Minutes:Sekundes:Milisekundes) : {0:00}:{1:00}:{2:00}.{3}",
+                            ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Uzd1
+{
+    internal class Matematika
+    {
+        private double max, sk1,laikinas;
+        private int sk2;
+
+        public double getMax()
+        {
+            return max;
+        }
+        public void setMax(double max)
+        {
+            this.max = max;
+        }
+        public double getSk1()
+        {
+            return sk1;
+        }
+        public void setSk1(double sk1)
+        {
+            this.sk1 = sk1;
+        }
+        public int getSk2()
+        {
+            return sk2;
+        }
+        public void setSk2(int sk2)
+        {
+            this.sk2 = sk2;
+        }
+        public Matematika(double max, double sk1, int sk2)
+        {
+            this.max = max;
+            this.sk1 = sk1;
+            this.sk2 = sk2;
+        }
+        public void Skaiciavimas()
+        {
+            //1 2 2 4 8 32
+            // 2 2 4 8 32
+            // 2 4 8 32
+            int skaiciuokle = 0;
+            double laikinas = 1;
+
+            while (this.sk1 < 32)
+            {   
+                // laikinas = 1 * 2
+                // sk1 = 2 * 1
+                //laikinas = 2 * 2
+                // sk1 = 4 * 2
+                //laikinas = 8 * 4
+                //sk1 = 8 * 16
+
+                this.laikinas = this.sk1 * this.laikinas;
+                this.sk1 = this.laikinas * this.sk1;
+                skaiciuokle++;
+            }
+            Console.WriteLine("Kiek kartu teko skaiciuoti? : " + skaiciuokle + " Koks galutinis skaicius? : " + this.sk1);
+        }
+
+        
+    }
+}
