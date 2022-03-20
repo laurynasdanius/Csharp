146a317ec91c05814b93b5f3a88ce076d6a06910
Laurynas Danius <laurynas.danius1@gmail.com>
2022-03-20 21:18:30 +0200

    fixed comment

--- KD4/KD4/MainClass.cs
+++ KD4/KD4/MainClass.cs
@@ -12,7 +12,7 @@ namespace KD4
         {
             Byla byla = new Byla("Failai", 5);
             byla.kurti();
-            //kvieciamas ataskaitos metodas su parametrais kaip aplankalo kuriame bus ataskaitos aplankalas ir pacios ataskaitos aplankalo pavadinimas
+            //kvieciamas ataskaitos metodas su parametru ataskaitos aplankalo pavadinimas
             byla.ataskaita("Ataskaita");
             //kvieciamas trinimo metodas, kuris istrins failus kuriuose yra 0, taciau ataskaita neatnaujinama..
             //byla.trinti();
