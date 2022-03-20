cc3c8aca4144e11d1614642dcb34bfadd822f67e
Laurynas Danius <laurynas.danius1@gmail.com>
2022-03-20 20:25:29 +0200

    Finished ataskaita,trinti,kurti methods, new main file with main function, added comments

---
+++ KD4/KD4/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KD4
+{
+    internal class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Byla byla = new Byla("Failai", 5);
+            byla.kurti();
+            //kvieciamas ataskaitos metodas su parametrais kaip aplankalo kuriame bus ataskaitos aplankalas ir pacios ataskaitos aplankalo pavadinimas
+            //byla.ataskaita(byla.folderPath,"Ataskaita");
+            //kvieciamas trinimo metodas, kuris istrins failus kuriuose yra 0, taciau ataskaita neatnaujinama..
+            //byla.trinti();
+
+
+        }
+    }
+}
